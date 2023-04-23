f8cca273c128f29e30dd494b6b918029b0bcea69
LittleTurtle2333 <lturtle2333@outlook.com>
2023-04-23 15:03:51 +0800

    实现Display显示排队信息

--- src/main/java/org/example/Display.java
+++ src/main/java/org/example/Display.java
@@ -3,47 +3,36 @@ package org.example;
 
 import com.display.IDisplay;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Display implements IDisplay {
 
-    String number;//当前取号
+    static String queue;
 
-    String windowName;//办理窗口名称
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(InetAddress.getLocalHost(),
+                8989)) {
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            dos.writeUTF("all");
 
-    String queue;//排队信息
+            queue = dis.readUTF();
+            new Display().Show();
+
+            dos.close();
+            dis.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void Show() {
-        System.out.println("----------显示大屏排队信息----------");
+        System.out.println("----------显示排队信息----------");
         System.out.println(queue);
-    }
-
-    //菜单
-    public void menu(){
-        System.out.println("----------欢迎使用银行取号系统----------\n1.取号\n0.退出");
-    }
-
-    //取号，姓名：张三，您的取号是：12号
-    public void GetNumber(){
-        System.out.println("您的取号是："+ number +"号");
-    }
-
-    //叫号，请12号张三到 窗口1 办理业务
-    public void CallNumber(){
-        System.out.println("请"+ number +"号到 "+windowName+" 办理业务");
-    }
-
-    //过号，12号张三，已过号
-    public void OverNumber(){
-        System.out.println(number +"号，已过号");
-    }
-
-    //消费，正在为12号张三客户办理业务
-    /*public void EatNumber(){
-        System.out.println("正在为"+ number +"号客户办理业务");
-    }*/
-
-    //消费完成提示
-    public void EatSuccess(){
-        System.out.println(number +"号，您办理的业务已完成");
     }
 }
