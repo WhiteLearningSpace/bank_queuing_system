2f0c58dba401ecaabd86f78b4d3bda2deddc17e4
yhui <luo218yfabgz@qq.com>
2023-04-25 21:22:27 +0800

    实现Display显示叫号信息

--- display/src/org/dispexample/Main.java
+++ display/src/org/dispexample/Main.java
@@ -1,7 +1,50 @@
 package org.dispexample;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Display d =new Display();
+
+        //循环显示
+        while (true) {
+            Display.setQueue(getSeverMags("all"));
+            d.Show();
+
+            Display.setNowCalling(getSeverMags("getCalling"));
+            d.callingNumber();
+
+            //d.statistics();
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static String getSeverMags(String str){
+        String mgs;
+        try (Socket socket = new Socket(InetAddress.getLocalHost(),
+                8989)) {
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            dos.writeUTF(str);
+
+            mgs = dis.readUTF();
+
+            dos.close();
+            dis.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return mgs;
     }
 }
