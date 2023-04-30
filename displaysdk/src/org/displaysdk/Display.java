295cefa7ff2425f8d619ede6993b6f35ab8a8c15
LinWhite2333 <LinWhite2333@outlook.com>
2023-04-30 21:16:45 +0800

    修改display和displaysdk模块

--- displaysdk/src/org/displaysdk/Display.java
+++ displaysdk/src/org/displaysdk/Display.java
@@ -1,9 +1,40 @@
 package org.displaysdk;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Display implements IDisplay {
+    public static void main(String[] args) {
+        new Display().show();
+    }
 
     @Override
     public void show() {
+        getQueueInfo();
+    }
 
+    /**
+     *
+     */
+    private void getQueueInfo() {
+        try (Socket socket = new Socket(InetAddress.getLocalHost(),
+                                        8989)) {
+            PrintStream ps = new PrintStream(socket.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            ps.println("addOnlineMonitor");
+
+            String readStr;
+            while ((readStr = br.readLine()) != null) {
+                System.out.println(readStr);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
