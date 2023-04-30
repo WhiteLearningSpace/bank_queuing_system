db47f1df90225e67bf7088a363ad28ebeb1a5e67
LinWhite2333 <LinWhite2333@outlook.com>
2023-04-30 16:53:57 +0800

    修改叫号器

--- caller/src/org/callexample/Caller.java
+++ caller/src/org/callexample/Caller.java
@@ -1,74 +1,113 @@
 package org.callexample;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Caller {
+    Scanner scanner = new Scanner(System.in);
+    private int counterID;
+
     public Caller() {
-        Scanner scanner = new Scanner(System.in);
-        byte counterID;
+        createCaller();
+        waitCall();
+    }
 
+
+    private void waitCall() {
         while (true) {
-            System.out.println("请输入柜台号码：");
-            counterID = scanner.nextByte();
+            System.out.print("是否叫号(1-叫号、*-退出): ");
+            int isCall = scanner.nextInt();
+            if (isCall == 1) {
+                removeNumber();
+            } else {
+                break;
+            }
+        }
+    }
 
-            //传递柜台号码
+    /**
+     * 创建柜台ID并判断是否成功
+     */
+    private void createCaller() {
+        while (true) {
+            // 输入柜台ID
+            System.out.print("请输入柜台号码(数字)： ");
+            try {
+                counterID = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("参数错误，只能输入数字");
+            }
+
+            // 创建柜台ID并判断是否成功
             try (Socket socket = new Socket(InetAddress.getLocalHost(),
                                             8989)) {
+                PrintStream ps = new PrintStream(socket.getOutputStream());
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                DataInputStream dis = new DataInputStream(socket.getInputStream());
-                dos.writeUTF("createCaller&" + counterID);
-
-                if (dis.readUTF()
-                       .equals("失败")) {//未到场，过号处理
+                ps.println("createCaller&" + counterID);
+                if (br.readLine()
+                      .equals("失败")) {// 柜台已存在
                     System.out.println("柜台已在别处登录");
                     continue;
                 }
+                System.out.println("创建柜台ID" + counterID);
 
-                dos.close();
-                dis.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
             break;
         }
+    }
 
-        while (true) {
-            try (Socket socket = new Socket(InetAddress.getLocalHost(),
-                                            8989)) {
-                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                DataInputStream dis = new DataInputStream(socket.getInputStream());
+    /**
+     * 删除号码并判断用户是否到场，过号就将号码放回队列的末尾
+     */
+    private void removeNumber() {
+        try (Socket socket = new Socket(InetAddress.getLocalHost(),
+                                        8989)) {
+            PrintStream ps = new PrintStream(socket.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                dos.writeUTF("removeNumber&" + counterID);
-                String num = dis.readUTF();
-
-
-                System.out.println("用户是否到场？");
-                checkIn(scanner.nextInt() != 0,
-                        num);
-
-                dos.close();
-                dis.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            // 删除号码
+            ps.println("removeNumber&" + counterID);
+            String num = br.readLine();
+            if (num.equals("null")) {
+                System.out.println("队列为空");
+                return;
             }
+            // 判断用户是否到场
+            // 过号就将号码放回队列的末尾
+            System.out.print("用户是否到场(1-到场、*-未到场)？");
+            boolean isPresent = scanner.nextInt() == 1;
+            checkIn(isPresent,
+                    Integer.parseInt(num));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void checkIn(Boolean bool, String str) {
+    /**
+     * 判断用户是否到场
+     *
+     * @param isPresent - 是否到场
+     * @param num       - 号码
+     */
+    private void checkIn(boolean isPresent, int num) {
         try (Socket socket = new Socket(InetAddress.getLocalHost(),
                                         8989)) {
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            DataInputStream dis = new DataInputStream(socket.getInputStream());
-            dos.writeUTF("checkIn&" + bool + "&" + str);
+            PrintStream ps = new PrintStream(socket.getOutputStream());
 
-            dos.close();
-            dis.close();
+            // 判断用户是否到场
+            ps.println("checkIn&" + isPresent + "&" + num);
+            String isPresentStr = isPresent ? "在场，处理业务" : "不在场，过号";
+            System.out.println("用户" + isPresentStr);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
