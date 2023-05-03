a2320603bfb73c73e155358ae8cd5efc0b6ac134
Linwhite2333 <linwhite2333@outlook.com>
2023-05-03 18:03:41 +0800

    取号器添加设置ip和端口号功能

--- getter/src/org/getexample/Main.java
+++ getter/src/org/getexample/Main.java
@@ -8,17 +8,28 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static InetAddress IP;
+    public static int PORT;
+
+    public static void main(String[] args) throws UnknownHostException {
+        if (args.length == 2) {
+            IP = InetAddress.getByName(args[0]);
+            PORT = Integer.parseInt(args[1]);
+        } else {
+            IP = InetAddress.getLocalHost();
+            PORT = 8989;
+        }
         while (true) {
             GlobalUtils.clearCMD();
             System.out.println("输入: 1-取号");
             Scanner sc = new Scanner(System.in);
             String s = sc.nextLine();
             if (s.equals("1")) {
-                try (Socket socket = new Socket(InetAddress.getLocalHost(), 8989)) {
+                try (Socket socket = new Socket(IP, PORT)) {
 
                     PrintStream ps = new PrintStream(socket.getOutputStream());
                     BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
