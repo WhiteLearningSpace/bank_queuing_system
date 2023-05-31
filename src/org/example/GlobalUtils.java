57a41ff0af48db508bdfbc037fef1bd72103d6b9
Linwhite2333 <linwhite2333@outlook.com>
2023-05-31 13:17:32 +0800

    添加发送关机信号`sendShutdownSignal()`方法

--- src/org/example/GlobalUtils.java
+++ src/org/example/GlobalUtils.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class GlobalUtils {
     public static void clearCMD() {
@@ -18,5 +19,22 @@ public class GlobalUtils {
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * 发送关机信号
+     * 用多线程监听键盘输入是q或quit就关闭程序
+     */
+    public static void sendShutdownSignal() {
+        new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String input = scanner.nextLine().trim().toLowerCase();
+                if (input.equals("q") || input.equals("quit")) {
+                    System.exit(0);
+                    break;
+                }
+            }
+        }).start();
     }
 }
